ee6b39e05e7a0d4c46feb8910000f10e946161b1
Thais Rosa <tsrosa18@gmail.com>
2023-04-14 01:28:56 -0300

    fix: tentando corrigir erro requisito 8

--- src/controllers/controllerCategory.js
+++ src/controllers/controllerCategory.js
@@ -1,12 +1,12 @@
 const serviceCategory = require('../services/serviceCategory');
 
 const categoryConfig = async (req, res) => {
-  const { body: { name } } = req;
-  const { message } = await serviceCategory.categoryConfig(name);
-  return res.status(201).json(message);
+    const { body: { name } } = req;
+    const { message } = await serviceCategory.categoryConfig(name);
+    return res.status(201).json(message);
 };
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
     const result = await serviceCategory.getAll();
     return res.status(200).json(result);
   };
